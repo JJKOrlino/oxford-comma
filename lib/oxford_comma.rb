6aaed04b3970407577ab12907e659c977192aec5
JJKOrlino <jjkorlino80@yahoo.com>
2020-11-14 21:24:16 -0600

    commit tree f4a2072e002ff9adf46002f056e4ec342af84228

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
+# line 7-8 only one word
+# line 9-10 two words, join with "and" in middle
+# line 11-12 three words, -1 for input before last
 
-end
+
+def oxford_comma(array)
+    if array.length == 1
+      return "#{array[0]}"
+    elsif array.length == 2
+      return array.join (" and ")
+    elsif array.length >= 3
+      array[-1] = "and #{array[-1]}"
+      return array.join (", ")
+    end
+  end
